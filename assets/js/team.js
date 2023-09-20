5fb2a8dceb1615b80149c9c756827fc74bb0621f
sameer0321 <nitinsainsrowa@gmail.com>
2023-09-20 10:45:05 +0530

    space managed

--- assets/js/team.js
+++ assets/js/team.js
@@ -1,12 +1,23 @@
 let mobileView = document.querySelector(".mobileView");
 let menu = document.querySelector(".menu");
 let body = document.querySelector("body");
-let navlinks = document.querySelector("ul")
+let navlinks = document.querySelector("ul");
+let span1 = document.querySelector(".span1");
+let span2 = document.querySelector(".span2");
+let span3 = document.querySelector(".span3");
 
-menu.addEventListener("click",function(){
-  mobileView.classList.toggle("show");
-  body.classList.toggle("overflow-hidden");
+menu.addEventListener("click", function () {
+    mobileView.classList.toggle("show");
+    body.classList.toggle("overflow-hidden");
+    span1.classList.toggle("rotate1");
+    span2.classList.toggle("rotate2");
+    span3.classList.toggle("rotate3");
+
+
 })
-navlinks.addEventListener("click",function(){
+navlinks.addEventListener("click", function () {
     navlinks.classList.remove("show");
+    span1.classList.toggle("rotate1");
+    span2.classList.toggle("rotate2");
+    span3.classList.toggle("rotate3");
 })
